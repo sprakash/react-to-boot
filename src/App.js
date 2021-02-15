873726bb5ebd159a923eb790c31a639b693de448
unknown <prakashnitza@gmail.com>
2021-02-15 21:20:26 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Custom from "./components/Custom";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Custom />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Custom.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Custom = () => {
+  return <div>hey we gonna react bootstrap</div>;
+};
+
+export default Custom;
